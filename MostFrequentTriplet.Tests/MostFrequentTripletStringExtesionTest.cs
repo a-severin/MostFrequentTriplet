e34abb3ef2e7b35ef786de0ba64b4a3a1e46873b
aseverin <severin.a.u@live.ru>
2016-09-28 22:07:45 +0300

    task complete

---
+++ MostFrequentTriplet.Tests/MostFrequentTripletStringExtesionTest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using MostFrequentTriplet.Tests.Properties;
+using NUnit.Framework;
+
+namespace MostFrequentTriplet.Tests {
+    [TestFixture]
+    public class MostFrequentTripletStringExtesionTest {
+        [Test]
+        [TestCase("asdqweasdqweasdzxc", 0, "asd", 3)]
+        [TestCase("asdqweasdqweasdzxc", 1, "sdq", 2)]
+        [TestCase("asdqweasdqweasdzxc", 2, "dqw", 2)]
+        public void CollectTriplets_ReturnRightMap(string str, int startIndex, string mostFrequentTriplet, int count) {
+            var map = MostFrequentTripletStringExtesion.CollectTriplets(startIndex, str, CancellationToken.None);
+            var mostFrequentPair = map.OrderByDescending(_ => _.Value.Value).First();
+
+            Assert.AreEqual(mostFrequentTriplet, mostFrequentPair.Key);
+            Assert.AreEqual(count, mostFrequentPair.Value.Value);
+        }
+
+        [Test]
+        public void MostFrequentTriplet_Benchmark() {
+            var sw = Stopwatch.StartNew();
+            var result = Resources.str.MostFrequentTriplet(CancellationToken.None);
+            var elapsed = sw.Elapsed;
+            sw.Stop();
+            Console.WriteLine(elapsed);
+            Console.WriteLine(result);
+            Assert.True(elapsed < TimeSpan.FromMilliseconds(100));
+        }
+
+        [Test]
+        public void MostFrequentTriplet_ReturnRightResult() {
+            var result = "asdqweasdqweasdzxc".MostFrequentTriplet(CancellationToken.None);
+
+            Assert.AreEqual("asd\t3", result);
+        }
+
+        [Test]
+        public void MostFrequentTriplet_ThrowOperationCanceledException_OnCancel() {
+            var cts = new CancellationTokenSource(100);
+            cts.Cancel();
+            Assert.Throws<OperationCanceledException>(() => Resources.str.MostFrequentTriplet(cts.Token));
+        }
+
+        [Test]
+        public void MostFrequentTriplet2_ReturnEmptyString_OnCancel() {
+            var cts = new CancellationTokenSource(100);
+            var sw = Stopwatch.StartNew();
+            cts.CancelAfter(10);
+            var result = Resources.str.MostFrequentTriplet2(cts.Token);
+            var elapsed = sw.Elapsed;
+            sw.Stop();
+            Assert.IsEmpty(result);
+            Console.WriteLine(elapsed);
+            Console.WriteLine(result);
+            Assert.True(elapsed < TimeSpan.FromMilliseconds(20));
+        }
+    }
+}
---
+++ MostFrequentTriplet/MostFrequentTripletStringExtesion.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MostFrequentTriplet {
+    public static class MostFrequentTripletStringExtesion {
+        public static Dictionary<string, Count> CollectTriplets(int startIndex, string str, CancellationToken ct) {
+            ct.ThrowIfCancellationRequested();
+            var map = new Dictionary<string, Count>();
+            for (var i = startIndex; i < str.Length - 2; i += 3) {
+                var triplet = str.Substring(i, 3);
+                Count count;
+                if (map.TryGetValue(triplet, out count)) {
+                    count.Value++;
+                } else {
+                    map.Add(triplet, new Count());
+                }
+                ct.ThrowIfCancellationRequested();
+            }
+            return map;
+        }
+
+        public static string MostFrequentTriplet(this string str, CancellationToken ct) {
+            ct.ThrowIfCancellationRequested();
+
+            var task0 = Task.Run(() => CollectTriplets(0, str, ct), ct);
+            var task1 = Task.Run(() => CollectTriplets(1, str, ct), ct);
+            var task2 = Task.Run(() => CollectTriplets(2, str, ct), ct);
+
+            var result = Task.WhenAll(task0, task1, task2).Result;
+
+            ct.ThrowIfCancellationRequested();
+
+            var max = 0;
+            var triplest = new List<string>();
+            foreach (var pair in result.SelectMany(_ => _)) {
+                if (max > pair.Value.Value) {
+                    continue;
+                }
+                if (max == pair.Value.Value) {
+                    triplest.Add(pair.Key);
+                }
+                if (max < pair.Value.Value) {
+                    triplest.Clear();
+                    triplest.Add(pair.Key);
+                    max = pair.Value.Value;
+                }
+
+                ct.ThrowIfCancellationRequested();
+            }
+
+            return $"{string.Join(",", triplest.Distinct())}\t{max}";
+        }
+
+        public static string MostFrequentTriplet2(this string str, CancellationToken ct) {
+            if (ct.IsCancellationRequested) {
+                return string.Empty;
+            }
+
+            var task0 = Task.Run(() => CollectTriplets(0, str, ct), ct);
+            var task1 = Task.Run(() => CollectTriplets(1, str, ct), ct);
+            var task2 = Task.Run(() => CollectTriplets(2, str, ct), ct);
+
+            Dictionary<string, Count>[] result;
+            try {
+                result = Task.WhenAll(task0, task1, task2).Result;
+            } catch (AggregateException e) {
+                if (e.InnerException is OperationCanceledException) {
+                    return string.Empty;
+                }
+                throw;
+            }
+
+            var max = 0;
+            var triplest = new List<string>();
+            foreach (var pair in result.SelectMany(_ => _)) {
+                if (max > pair.Value.Value) {
+                    continue;
+                }
+                if (max == pair.Value.Value) {
+                    triplest.Add(pair.Key);
+                }
+                if (max < pair.Value.Value) {
+                    triplest.Clear();
+                    triplest.Add(pair.Key);
+                    max = pair.Value.Value;
+                }
+
+                if (ct.IsCancellationRequested) {
+                    return string.Empty;
+                }
+            }
+
+            return $"{string.Join(",", triplest.Distinct())}\t{max}";
+        }
+
+        public class Count {
+            public int Value = 1;
+        }
+    }
+}
